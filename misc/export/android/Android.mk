6e1eadf58ae5f8ebaecee868f5d1889ac92cce18
exportserver <exportserver@citrine-lang.org>
2025-04-24 16:11:54 +0200

    adjust android mk

--- misc/export/android/Android.mk
+++ misc/export/android/Android.mk
@@ -4,28 +4,33 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
 
-SDL_PATH := ../SDL
-ISO := <Your Language Code>
+SDL_PATH := ../SDL2
+ISO := en
 
 # Create a symlink called orig that points to the Citrine source directory
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/.. $(LOCAL_PATH)/../Citrine/orig $(LOCAL_PATH)/../Citrine/orig/plugins/media \
-$(LOCAL_PATH)/../Citrine/orig/i18n/$(ISO) \
-$(LOCAL_PATH)/../Citrine/orig/plugins/media/i18n/$(ISO)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/.. $(LOCAL_PATH)/../Citrine/orig/src \
+$(LOCAL_PATH)/../Citrine/orig/src/i18n/$(ISO) \
+$(LOCAL_PATH)/../Citrine/orig/src/plugins/gui \
+$(LOCAL_PATH)/../Citrine/orig/src/plugins/gui/i18n/$(ISO) \
+
 
 # Add your application source files here...
-LOCAL_SRC_FILES :=  citrine.c  orig/base.c orig/file.c \
-orig/memory.c orig/siphash.c orig/util.c \
-orig/citrine.c orig/lexer.c orig/parser.c \
-orig/system.c orig/translator.c orig/walker.c \
-orig/collections.c  orig/portability.c orig/test.c \
-orig/utf8.c orig/world.c orig/plugins/media/media.c
+LOCAL_SRC_FILES :=  citrine_gui.c orig/src/base.c orig/src/file.c \
+orig/src/memory.c orig/src/siphash.c orig/src/util.c \
+orig/src/citrine.c orig/src/lexer.c orig/src/parser.c \
+orig/src/system.c orig/src/walker.c \
+orig/src/collections.c  orig/src/portability.c orig/src/test.c \
+orig/src/utf8.c orig/src/world.c \
+orig/src/plugins/gui/jsmn/jsmn.c orig/src/plugins/gui/json.c \
+orig/src/plugins/gui/vault.c orig/src/plugins/gui/gui.c\
 
-LOCAL_CFLAGS += -D SDL -D EXPORT_ANDROID -D FULLSCREEN
 
-$(orig/*.c)
+GUI_PATH = $(LOCAL_PATH)/orig/src/plugins/gui
+LOCAL_CFLAGS += -I$(GUI_PATH)
+LOCAL_CFLAGS += -D ANDROID_EXPORT -D FULLSCREEN
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf
 
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf SDL2_image lvgl
 LOCAL_LDLIBS := -lOpenSLES -llog -landroid 
 
 include $(BUILD_SHARED_LIBRARY)
