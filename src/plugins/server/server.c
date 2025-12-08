6fad186f7338b4ce07213eecdda38bbca1cb93b3
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2025-12-08 12:29:53 +0100

    add html encoder

--- src/plugins/server/server.c
+++ src/plugins/server/server.c
@@ -43,12 +43,65 @@ ctr_object* ctr_server_new(ctr_object* myself, ctr_argument* argumentList) {
 	return instance;
 }
 
+ctr_object* ctr_server_htmlencode_set(ctr_object* myself, ctr_argument* argumentList) {
+	char* source = ctr_heap_allocate_cstring(
+		ctr_internal_cast2string(argumentList->object)
+	);
+    size_t len = 0;
+	char* p;
+    for (p = source; *p; p++) {
+        switch (*p) {
+            case '&':  len += 5; break;  // &amp;
+            case '<':  len += 4; break;  // &lt;
+            case '>':  len += 4; break;  // &gt;
+            case '"':  len += 6; break;  // &quot;
+            case '\'': len += 6; break;  // &#039;
+            default:   len += 1; break;
+        }
+    }
+    char* dest = ctr_heap_allocate(len + 1);
+	if (!dest) {
+		ctr_error("Out of memory while creating html output buffer.", 0);
+		ctr_heap_free(source);
+		return CtrStdNil;
+	}
+    char *o = dest;
+    for (p = source; *p; p++) {
+        switch (*p) {
+            case '&':
+                memcpy(o, "&amp;", 5);  o += 5;
+                break;
+            case '<':
+                memcpy(o, "&lt;", 4);   o += 4;
+                break;
+            case '>':
+                memcpy(o, "&gt;", 4);   o += 4;
+                break;
+            case '"':
+                memcpy(o, "&quot;", 6); o += 6;
+                break;
+            case '\'':
+                memcpy(o, "&#039;", 6);  o += 6;
+                break;
+            default:
+                *o++ = *p;
+        }
+    }
+    *o = '\0';
+    ctr_object* dest_obj = ctr_build_string_from_cstring(dest);
+    ctr_heap_free(dest);
+    ctr_heap_free(source);
+    return dest_obj;
+}
+
+
 void begin() {
 	ctr_internal_server_init();
 	serverObject = NULL;
 	serverObject = ctr_server_new(CtrStdObject, NULL);
 	serverObject->link = CtrStdObject;
 	ctr_internal_create_func(serverObject, ctr_build_string_from_cstring( CTR_DICT_NEW ), &ctr_server_new );
+	ctr_internal_create_func(serverObject, ctr_build_string_from_cstring( "html-encode:" ), &ctr_server_htmlencode_set );
 	ctr_internal_object_add_property(CtrStdWorld, ctr_build_string_from_cstring( "Server" ), serverObject, CTR_CATEGORY_PUBLIC_PROPERTY);
 	#ifdef LIBCURL
 	begin_net();
