c6898a60785368d2830efbaac9da405d7808827d
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2026-01-04 16:15:23 +0100

    remove misc

--- misc/export/android/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.citrine.citrineandroid;
-import org.libsdl.app.SDLActivity;
-
-
-public class MainActivity extends SDLActivity {
-    // Used to load the 'citrineandroid' library on application startup.
-}
