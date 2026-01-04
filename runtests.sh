fffff5c601ff17becab82726d0f3a6da4e2f9271
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2026-01-04 20:00:30 +0100

    add regex test

--- runtests.sh
+++ runtests.sh
@@ -95,7 +95,7 @@ unittest() {
 
 # select range
 FROM=1
-TIL=629
+TIL=630
 
 # run tests for linux
 pushd build/Linux/bin
--- runtests_obsd.sh
+++ runtests_obsd.sh
@@ -79,7 +79,7 @@ unittest() {
 
 # select range
 FROM=1
-TIL=629
+TIL=630
 
 # run tests for linux
 pushd build/OpenBSD/bin
