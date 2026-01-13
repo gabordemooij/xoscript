9bcdad16cf0b1228947313fe631039f387dd9f91
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2026-01-13 14:42:43 +0100

    add passthru

--- src/plugins/server/server.c
+++ src/plugins/server/server.c
@@ -248,6 +248,203 @@ ctr_object* ctr_server_link_set(ctr_object* myself, ctr_argument* argumentList) 
 	return myself;
 }
 
+#define CTR_SERVER_MIMETYPE_UNKNOWN 0
+#define CTR_SERVER_MIMETYPE_JPEG    1
+#define CTR_SERVER_MIMETYPE_GIF     2
+#define CTR_SERVER_MIMETYPE_PNG     3
+#define CTR_SERVER_MIMETYPE_PDF     4
+#define CTR_SERVER_MIMETYPE_ZIP     5
+#define CTR_SERVER_MIMETYPE_GZIP    6
+#define CTR_SERVER_MIMETYPE_MP3     7
+#define CTR_SERVER_MIMETYPE_MP4     8
+#define CTR_SERVER_MIMETYPE_WEBP    9
+#define CTR_SERVER_MIMETYPE_WEBM    10
+
+
+int ctr_internal_server_detect_mimetype(const unsigned char *buf, size_t len) {
+
+    if (len < 4) return CTR_SERVER_MIMETYPE_UNKNOWN;
+
+    /* JPEG / JPG */
+    if (len >= 3 &&
+        buf[0] == 0xFF &&
+        buf[1] == 0xD8 &&
+        buf[2] == 0xFF) {
+        return CTR_SERVER_MIMETYPE_JPEG;
+    }
+
+    /* GIF */
+    if (len >= 6 &&
+        (!memcmp(buf, "GIF87a", 6) ||
+         !memcmp(buf, "GIF89a", 6))) {
+        return CTR_SERVER_MIMETYPE_GIF;
+    }
+
+    /* PNG */
+    if (len >= 8 &&
+        !memcmp(buf, "\x89PNG\r\n\x1A\n", 8)) {
+        return CTR_SERVER_MIMETYPE_PNG;
+    }
+
+    /* PDF */
+    if (len >= 5 &&
+        !memcmp(buf, "%PDF-", 5)) {
+        return CTR_SERVER_MIMETYPE_PDF;
+    }
+
+    /* ZIP */
+    if (len >= 4 &&
+        !memcmp(buf, "PK\x03\x04", 4)) {
+        return CTR_SERVER_MIMETYPE_ZIP;
+    }
+
+    /* GZIP */
+    if (len >= 2 &&
+        buf[0] == 0x1F &&
+        buf[1] == 0x8B) {
+        return CTR_SERVER_MIMETYPE_GZIP;
+    }
+
+    /* MP3 (ID3 tag or MPEG frame) */
+    if ((len >= 3 && !memcmp(buf, "ID3", 3)) ||
+        (len >= 2 && buf[0] == 0xFF && (buf[1] & 0xE0) == 0xE0)) {
+        return CTR_SERVER_MIMETYPE_MP3;
+    }
+
+    /* MP4 (ftyp box at offset 4) */
+    if (len >= 12 &&
+        !memcmp(buf + 4, "ftyp", 4)) {
+        return CTR_SERVER_MIMETYPE_MP4;
+    }
+
+    /* WEBP (RIFF + WEBP) */
+    if (len >= 12 &&
+        !memcmp(buf, "RIFF", 4) &&
+        !memcmp(buf + 8, "WEBP", 4)) {
+        return CTR_SERVER_MIMETYPE_WEBP;
+    }
+
+    /* WEBM (Matroska) */
+    if (len >= 4 &&
+        buf[0] == 0x1A &&
+        buf[1] == 0x45 &&
+        buf[2] == 0xDF &&
+        buf[3] == 0xA3) {
+        return CTR_SERVER_MIMETYPE_WEBM;
+    }
+
+    return CTR_SERVER_MIMETYPE_UNKNOWN;
+}
+
+#define MAX_ASSET_SIZE (50 * 1024 * 1024)
+ctr_object* ctr_server_passthru_set(ctr_object* myself, ctr_argument* argumentList) {
+	char* pathstr;
+	char* prefixstr;
+	char peekbuf[12];
+	prefixstr = ctr_heap_allocate_cstring(
+		ctr_internal_cast2string(argumentList->next->object)
+	);
+	pathstr = ctr_heap_allocate_cstring(
+		ctr_internal_cast2string(argumentList->object)
+	);
+	if (strchr(pathstr, '/')!=NULL || strstr(pathstr,"..")!=NULL) {
+		ctr_heap_free(pathstr);
+		ctr_heap_free(prefixstr);
+		ctr_error("Illegal path", 0);
+		return CtrStdNil;
+	}
+	int dirfd = open(prefixstr, O_RDONLY | O_DIRECTORY);
+	ctr_heap_free(prefixstr);
+	if (dirfd < 0) {
+		ctr_heap_free(pathstr);
+		ctr_error("Unable to open prefix", 0);
+		return CtrStdNil;
+	}
+	int fd = openat(dirfd, pathstr, O_RDONLY | O_NOFOLLOW);
+	close(dirfd);
+	ctr_heap_free(pathstr);
+	if (fd < 0) {
+		ctr_error("Unable to open path", 0);
+		return CtrStdNil;
+	}
+	struct stat st;
+	if (fstat(fd, &st) < 0  || !S_ISREG(st.st_mode) || st.st_size > MAX_ASSET_SIZE) {
+		close(fd);
+		ctr_error("Unable to get filesize or too big", 0);
+		return CtrStdNil;
+	}
+	size_t r = pread(fd, peekbuf, 12, 0);
+	if (r < 12) {
+		close(fd);
+		ctr_error("Unable to scan mimetype", 0);
+		return CtrStdNil;
+	}
+	int mimetype = ctr_internal_server_detect_mimetype(peekbuf, r);
+	if (mimetype == CTR_SERVER_MIMETYPE_UNKNOWN) {
+		// mimetype must be in whitelist
+		close(fd);
+		ctr_error("Unable to scan mimetype", 0);
+		return CtrStdNil;
+	}
+	switch(mimetype) {
+		case CTR_SERVER_MIMETYPE_JPEG:
+			printf("Content-Type: image/jpeg\r\n");
+			break;
+		case CTR_SERVER_MIMETYPE_PNG:
+			printf("Content-Type: image/png\r\n");
+			break;
+		case CTR_SERVER_MIMETYPE_GIF:
+			printf("Content-Type: image/gif\r\n");
+			break;
+		case CTR_SERVER_MIMETYPE_WEBP:
+			printf("Content-Type: image/webp\r\n");
+			break;
+		case CTR_SERVER_MIMETYPE_MP3:
+			printf("Content-Type: audio/mpeg\r\n");
+			break;
+		case CTR_SERVER_MIMETYPE_MP4:
+			printf("Content-Type: video/mp4\r\n");
+			break;
+		case CTR_SERVER_MIMETYPE_WEBM:
+			printf("Content-Type: video/webm\r\n");
+			break;
+		case CTR_SERVER_MIMETYPE_ZIP:
+			printf("Content-Type: application/zip\r\n");
+			printf("Content-Disposition: attachment\r\n");
+			break;
+		case CTR_SERVER_MIMETYPE_GZIP:
+			printf("Content-Type: application/gzip\r\n");
+			printf("Content-Disposition: attachment\r\n");
+			break;
+		case CTR_SERVER_MIMETYPE_PDF:
+			printf("Content-Type: application/pdf\r\n");
+			break;
+	}
+	printf("Content-Length: %lld\r\n", (long long)st.st_size);
+	printf("X-Content-Type-Options: nosniff\r\n\r\n");
+	fflush(stdout);
+	char buf[4096];
+	ssize_t n;
+	while ((n = read(fd, buf, sizeof buf)) > 0) {
+		ssize_t off = 0;
+		while (off < n) {
+			ssize_t w = write(STDOUT_FILENO, buf + off, n - off);
+			if (w < 0) {
+				ctr_error("Write error", 0);
+				goto out;
+			}
+			off += w;
+		}
+	}
+	if (n < 0) {
+		ctr_error("Read error", 0);
+		goto out;
+	}
+	out:
+	close(fd);
+	return myself;
+}
+
 void begin() {
 	ctr_internal_server_init();
 	serverObject = NULL;
@@ -259,6 +456,7 @@ void begin() {
 	ctr_internal_create_func(serverObject, ctr_build_string_from_cstring( "base64-encode:" ), &ctr_server_base64encode_set );
 	ctr_internal_create_func(serverObject, ctr_build_string_from_cstring( "base64-decode:" ), &ctr_server_base64decode_set );
 	ctr_internal_create_func(serverObject, ctr_build_string_from_cstring( "link:" ), &ctr_server_link_set );
+	ctr_internal_create_func(serverObject, ctr_build_string_from_cstring( "passthru:prefix:" ), &ctr_server_passthru_set );
 	ctr_internal_object_add_property(CtrStdWorld, ctr_build_string_from_cstring( "Server" ), serverObject, CTR_CATEGORY_PUBLIC_PROPERTY);
 	//@todo move to core
 	formatObject = NULL;
