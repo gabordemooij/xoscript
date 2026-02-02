cf8b283927e8621f483d638af436318db6559ba4
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2026-02-02 16:51:11 +0100

    add numberstr fuzztest

--- runtests.sh
+++ runtests.sh
@@ -101,7 +101,7 @@ unittest() {
 
 # select range
 FROM=1
-TIL=651
+TIL=652
 
 # run tests for linux
 pushd build/Linux/bin
--- runtests_obsd.sh
+++ runtests_obsd.sh
@@ -93,7 +93,7 @@ unittest() {
 
 # select range
 FROM=1
-TIL=651
+TIL=652
 
 # run tests for linux
 pushd build/OpenBSD/bin
