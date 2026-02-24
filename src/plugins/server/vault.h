563e771707609429b20319856abd0d32e410200f
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2026-02-24 16:49:10 +0100

    add note BASE64macro

--- src/plugins/server/vault.h
+++ src/plugins/server/vault.h
@@ -1,4 +1,6 @@
+//includes NUL byte
 #define BASE64_ENCODE_OUT_SIZE(s) (((((s) + 2) / 3) * 4) + 1)
+
 #define BASE64_DECODE_OUT_SIZE(s) (((s) / 4) * 3)
 
 unsigned int base64_encode(const unsigned char *in, unsigned int inlen, char *out);
