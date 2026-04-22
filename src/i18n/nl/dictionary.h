fee83973e78d01dd59151b7b47adb83e60713206
Gabor de Mooij <gabordemooij@gaborsoftware.nl>
2026-04-22 17:25:43 +0200

    wip i18n

--- src/i18n/nl/dictionary.h
+++ src/i18n/nl/dictionary.h
@@ -1,75 +1,60 @@
-#define CTR_DICT_CODEGEN_MAP_NEW                 "(Lijst nieuw) "
-#define CTR_DICT_CODEGEN_MAP_PUT                 "zet:"
-#define CTR_DICT_CODEGEN_MAP_PUT_AT              " bij:"
-#define CTR_DICT_CODEGEN_ARRAY_NEW               "Reeks nieuw "
-#define CTR_DICT_CODEGEN_ARRAY_NEW_PUSH          "Reeks ← "
-#define CTR_DICT_END_OF_LINE     "."
-#define CTR_DICT_NIL             "Niets"
-#define CTR_DICT_BOOLEAN         "Boolean"
-#define CTR_DICT_TRUE            "Ja"
-#define CTR_DICT_FALSE           "Nee"
-#define CTR_DICT_NUMBER          "Getal"
-#define CTR_DICT_STRING          "Tekst"
-#define CTR_DICT_TASK            "Taak"
-#define CTR_DICT_OBJECT          "Object"
-#define CTR_DICT_ARRAY_OBJECT    "Reeks"
-#define CTR_DICT_MAP_OBJECT      "Lijst"
-#define CTR_DICT_PROGRAM         "Programma"
-#define CTR_DICT_FILE            "Bestand"
-#define CTR_DICT_MOMENT          "Moment"
-#define CTR_DICT_VAR_ICON        ">>"
-#define CTR_DICT_ME_ICON         "zelf"
-#define CTR_DICT_MY_ICON         "eigen"
-#define CTR_DICT_BULLET          "~"
-#define CTR_DICT_SYMBOL_EQUALS   "="
-#define CTR_DICT_PLUS            "+"
-#define CTR_DICT_MINUS   	     "-"
-#define CTR_DICT_MULTIPLIER   	 "×"
-#define CTR_DICT_DIVISION   	 "÷"
-#define CTR_DICT_GREATER   	     ">"
-#define CTR_DICT_LESS   	     "<"
-#define CTR_DICT_AT_SYMBOL       "?" 
-#define CTR_DICT_PEN_ICON        "Uit"
-#define CTR_DICT_NEW_ARRAY_AND_PUSH_SYMBOL       "←"
-#define CTR_DICT_GREATER_OR_EQUAL_SYMBOL         "≥"
-#define CTR_DICT_LESS_OR_EQUAL_SYMBOL            "≤"
-#define CTR_DICT_UNEQUALS_SYMBOL                 "≠"
-#define CTR_DICT_NEW             "nieuw"
-#define CTR_DICT_EQUALS          "gelijk:"
-#define CTR_DICT_AND             "en:"
-#define CTR_DICT_OR              "of:"
-#define CTR_DICT_MODULO          "modulo:"
-#define CTR_DICT_NOR             "noch:"
-#define CTR_DICT_ONDO            "bij:doen:" 
-#define CTR_DICT_TYPE            "type" 
-#define CTR_DICT_ISNIL           "Niets?"
-#define CTR_DICT_MYSELF          "mijzelf"
-#define CTR_DICT_DO              "doen"
-#define CTR_DICT_DONE            "klaar"
-#define CTR_DICT_IFFALSE         "nee:"
-#define CTR_DICT_IFTRUE          "ja:"
-#define CTR_DICT_WHILE           "zolang:"
-#define CTR_DICT_MESSAGEARGS     "bericht:argumenten:"
-#define CTR_DICT_MESSAGE         "_bericht:"
-#define CTR_DICT_LEARN           "leer:betekent:"
-#define CTR_DICT_BREAK           "afbreken"
-#define CTR_DICT_CONTINUE        "doorgaan"
-#define CTR_DICT_ELSE            "anders:"
-#define CTR_DICT_NOT             "niet"
-#define CTR_DICT_TONUMBER        "getal"
-#define CTR_DICT_ITONUMBER       "intergetal"
-#define CTR_DICT_TOSTRING        "tekst"
-#define CTR_DICT_CHAR_AT_SET     "letter:"
-#define CTR_DICT_EITHEROR        "of:of:"
-#define CTR_DICT_BY_SET       	 "per:"
-#define CTR_DICT_FLOOR           "laag"
-#define CTR_DICT_CEIL            "hoog"
-#define CTR_DICT_ROUND           "afgerond"
-#define CTR_DICT_ABS             "absoluut"
-#define CTR_DICT_SQRT            "vierkantswortel"
-#define CTR_DICT_POWER           "macht:"
-#define CTR_DICT_MIN             "minimum" 
-#define CTR_DICT_MAX             "maximum"
+/* Dutch Language File for XOScript Core Module */
+
+/* Section A: Objects, Keywords & Messages (translation recommended) */
+#define CTR_DICT_NIL_OBJECT                      "Niets"
+#define CTR_DICT_BOOLEAN_OBJECT                  "Boolean"
+#define CTR_DICT_TRUE_OBJECT                     "Ja"
+#define CTR_DICT_FALSE_OBJECT                    "Nee"
+#define CTR_DICT_NUMBER_OBJECT                   "Getal"
+#define CTR_DICT_STRING_OBJECT                   "Tekst"
+#define CTR_DICT_FUNCTION_OBJECT                 "Functie"
+#define CTR_DICT_OBJECT_OBJECT                   "Object"
+#define CTR_DICT_ARRAY_OBJECT                    "Reeks"
+#define CTR_DICT_MAP_OBJECT                      "Lijst"
+#define CTR_DICT_PROGRAM_OBJECT                  "Programma"
+#define CTR_DICT_FILE_OBJECT                     "Bestand"
+#define CTR_DICT_MOMENT_OBJECT                   "Moment"
+#define CTR_DICT_INT64_OBJECT                    "Int64"
+#define CTR_DICT_PATH_OBJECT                     "Bestandslocatie"
+#define CTR_DICT_CMD_OBJECT                      "Instructie"
+#define CTR_DICT_STDOUT_OBJECT                   "Uit"
+#define CTR_DICT_SELF                            "zelf"
+#define CTR_DICT_OWN                             "eigen"
+#define CTR_DICT_NEW                             "nieuw"
+#define CTR_DICT_NEW_SET                         "nieuw:"
+#define CTR_DICT_NEW_TYPE_SET                    "nieuw:type:"
+#define CTR_DICT_EQUALS                          "gelijk:"
+#define CTR_DICT_AND                             "en:"
+#define CTR_DICT_OR                              "of:"
+#define CTR_DICT_MODULO                          "modulo:"
+#define CTR_DICT_NOR                             "noch:"
+#define CTR_DICT_ONDO                            "bij:doen:" 
+#define CTR_DICT_TYPE                            "type" 
+#define CTR_DICT_ISNIL                           "Niets?"
+#define CTR_DICT_MYSELF                          "mijzelf"
+#define CTR_DICT_DO                              "doen"
+#define CTR_DICT_DONE                            "klaar"
+#define CTR_DICT_IFFALSE                         "nee:"
+#define CTR_DICT_IFTRUE                          "ja:"
+#define CTR_DICT_WHILE                           "zolang:"
+#define CTR_DICT_MESSAGEARGS                     "bericht:argumenten:"
+#define CTR_DICT_MESSAGE                         "bericht:"
+#define CTR_DICT_LEARN                           "leer:betekent:"
+#define CTR_DICT_BREAK                           "afbreken"
+#define CTR_DICT_CONTINUE                        "doorgaan"
+#define CTR_DICT_ELSE                            "anders:"
+#define CTR_DICT_NOT                             "niet"
+#define CTR_DICT_TONUMBER                        "getal"
+#define CTR_DICT_TOSTRING                        "tekst"
+#define CTR_DICT_CHAR_AT_SET                     "letter:"
+#define CTR_DICT_EITHEROR                        "of:of:"
+#define CTR_DICT_BY_SET                          "per:"
+#define CTR_DICT_FLOOR                           "laag"
+#define CTR_DICT_CEIL                            "hoog"
+#define CTR_DICT_ROUND                           "afgerond"
+#define CTR_DICT_POWER                           "tot-de-macht:"
+#define CTR_DICT_MIN                             "minimum" 
+#define CTR_DICT_MAX                             "maximum"
 #define CTR_DICT_ODD   	         "oneven?"
 #define CTR_DICT_EVEN   	     "even?"
 #define CTR_DICT_POS   	         "positief?" 
@@ -88,11 +73,11 @@
 #define CTR_DICT_SKIP            "overslaan:" 
 #define CTR_DICT_RUN             "start" 
 #define CTR_DICT_APPLY_TO        "toepassen:" 
-#define CTR_DICT_APPLY_TO_AND    "toepassen:en:" 
+#define CTR_DICT_APPLY_TO_AND    "toepassen:en:"
+#define CTR_DICT_APPLY_TO_AND_AND        "toepassen:en:en:"
 #define CTR_DICT_VALUE_SET       "gebruik:waarde:" 
 #define CTR_DICT_ERROR	         "fout:" 
 #define CTR_DICT_CATCH           "afhandelen:" 
-#define CTR_DICT_PUSH_SYMBOL     ";" 
 #define CTR_DICT_SHIFT           "lknip"
 #define CTR_DICT_COUNT           "aantal" 
 #define CTR_DICT_JOIN            "samenvoegen:" 
@@ -120,8 +105,8 @@
 #define CTR_DICT_FLUSH           "spoelen"
 #define CTR_DICT_WAIT            "wacht:" 
 #define CTR_DICT_TIME	         "tijd" 
-#define CTR_DICT_RESPOND_TO      "bericht:"
-#define CTR_DICT_RESPOND_TO_AND  "bericht:argument:"
+#define CTR_DICT_RESPOND_TO      "reactie:"
+#define CTR_DICT_RESPOND_TO_AND  "reactie:argument:"
 #define CTR_DICT_SHELL           "opdrachtregel:"
 #define CTR_DICT_SWEEP           "vegen" 
 #define CTR_DICT_MEMORY_LIMIT	 "geheugen:"
@@ -131,7 +116,6 @@
 #define CTR_DICT_CHARACTERS      "letters"
 #define CTR_DICT_QUALIFIER_SET         "kwalificering:"
 #define CTR_DICT_QUALIFIER   "kwalificering"
-#define CTR_DICT_NEW_SET         "nieuw:"
 #define CTR_DICT_HOUR            "uur"
 #define CTR_DICT_HOUR_SET        "uur:"
 #define CTR_DICT_MINUTE_SET      "minuut:"
@@ -170,33 +154,82 @@
 #define CTR_DICT_ASCII_UPPER_CASE                "hoofdletters" 
 #define CTR_DICT_ASCII_LOWER_CASE                "kleine-letters"
 #define CTR_DICT_CONTAINS                        "bevat:"
-#define CTR_DICT_APPLY_TO_AND_AND                "toepassen:en:en:"
 #define CTR_DICT_ENVIRONMENT_VARIABLE            "instelling:"
 #define CTR_DICT_SET_ENVIRONMENT_VARIABLE        "instelling:is:" 
-#define CTR_DICT_RESPOND_TO_AND_AND              "bericht:argument:argument:"
-#define CTR_DICT_RESPOND_TO_AND_AND_AND          "bericht:argument:argument:argument:"
+#define CTR_DICT_RESPOND_TO_AND_AND              "reactie:argument:argument:"
+#define CTR_DICT_RESPOND_TO_AND_AND_AND          "reactie:argument:argument:argument:"
 #define CTR_DICT_CURRENT_TASK                    "deze-taak"
-#define CTR_DICT_NUM_DEC_SEP                     ","
-#define CTR_DICT_NUM_THO_SEP                     "."
-#define CTR_DICT_QUOT_OPEN                       "['"
-#define CTR_DICT_QUOT_CLOSE                      "']"
-#define CTR_DICT_MESSAGE_CHAIN                   ","
-#define CTR_DICT_ASSIGN                          ":="
-#define CTR_DICT_PAREN_OPEN                      "("
-#define CTR_DICT_PAREN_CLOSE                     ")"
-#define CTR_DICT_BLOCK_START                     "{"
-#define CTR_DICT_BLOCK_END                       "}"
-#define CTR_DICT_PARAMETER_PREFIX                ":"
-#define CTR_DICT_RETURN                          "<-"
 #define CTR_DICT_CODE                            "code"
 #define CTR_DICT_PROCEDURE                       "procedure"
 #define CTR_DICT_TOOBJECT        "object"
-#define CTR_DICT_PATH_OBJECT "Bestandslocatie"
-#define CTR_DICT_CMD_OBJECT  "Instructie"
 #define CTR_DICT_RECURSIVE "recursief"
-#define CTR_DICT_MATH_SIN                        "sin"
-#define CTR_DICT_MATH_COS                        "cos"
-#define CTR_DICT_MATH_TAN                        "tan"
-#define CTR_DICT_MATH_ATAN                       "atan"
-#define CTR_DICT_MATH_LOG                        "log"
 #define CTR_DICT_TIMEMACHINE_SET                 "tijdmachine:"
+
+
+/* Section B: Code generation, must match Dict/List messages! */
+#define CTR_DICT_CODEGEN_MAP_NEW                 "(Lijst nieuw) "
+#define CTR_DICT_CODEGEN_MAP_PUT                 "zet:"
+#define CTR_DICT_CODEGEN_MAP_PUT_AT              " bij:"
+#define CTR_DICT_CODEGEN_ARRAY_NEW               "Reeks nieuw "
+#define CTR_DICT_CODEGEN_ARRAY_NEW_PUSH          "Reeks ← "
+
+
+/* Section C: System terms & Math (hard to translate) */
+#define CTR_DICT_OBSD_PLEDGE_SET                 "pledge:"                   // Term to invoke pledge command on OpenBSD with argument
+#define CTR_DICT_OBSD_UNVEIL_SET                 "unveil:permissions:"       // Term to invoke unveil command on OpenBSD with arguments
+#define CTR_DICT_UNIX_CHMOD_SET                  "chmod:"                    // Word or abbreviation to apply a POSIX-style chmod command to the recipient object
+#define CTR_DICT_UNIX_STAT                       "stat"                      // Word to obtain the status of a POSIX-statable object
+#define CTR_DICT_UNIX_MKDIR_SET                  "mkdir:mode:"               // Phrase to create a new directory specified by the path in argument 1 with mode specified by argument 2
+#define CTR_DICT_UNIX_CHDIR_SET                  "chdir:"                    // Term to switch working directory to specified path in argument
+#define CTR_DICT_HEXADECIMAL                     "Hx"                        // Word or abbreviation indicating a hexadecimal number
+#define CTR_DICT_OCTAL                           "Oct"                       // Word or abbreviation indicating an octal number
+#define CTR_DICT_BINARY                          "Bin"                       // Word or abbreviation indicating a binary number
+#define CTR_DICT_UTF8                            "utf8"                      // Word or abbreviation to request a UTF-8 encoded string from the recipient object
+#define CTR_DICT_UTF8_SET                        "utf8:"                     // Word to set utf8 encoded data
+#define CTR_DICT_BYTES                           "bytes"                     // Word to obtain byte values from recipient object
+#define CTR_DICT_BYTES_SET                       "bytes:"                    // Word to change or set the bytes of an object (most likely Blob)
+#define CTR_DICT_BYTE                            "byte"                      // Word to obtain a single byte value from a recipient object
+#define CTR_DICT_TCCOMPARE                       "tccompare:"                // Word to compare the recipient object to the object specified by the argument using a constant time comparison
+#define CTR_DICT_TEMPORARY_WRITE_SET             "tempwrite:"                // Word, abbreviation or term to describe writing a string to a temporary file (path will be returned)
+#define CTR_DICT_OWNER_GROUP_SET                 "owner:group:"              // Phrase to change owner and group of a resource represented by the recipient object
+#define CTR_DICT_GC_MODE_GET                     "gcmode"                    // Word to obtain the current mode of operation of the garbage collector
+#define CTR_DICT_SHELLESC_SET                    "shell-escape:"             // Word or phrase to escape the string in the argument for use in a shell command
+#define CTR_DICT_ABS                             "abs"                       // Word for the absolute value without the sign
+#define CTR_DICT_SQRT                            "sqrt"                      // Word for the square root of a number
+#define CTR_DICT_MATH_SIN                        "sin"                       // Word or abbreviation for sinus
+#define CTR_DICT_MATH_COS                        "cos"                       // Word or abbreviation for cosinus
+#define CTR_DICT_MATH_TAN                        "tan"                       // Word or abbreviation for tangus
+#define CTR_DICT_MATH_ATAN                       "atan"                      // Word or abbreviation for atangus
+#define CTR_DICT_MATH_LOG                        "log"                       // Word or abbreviation for logarithm
+
+/* Section D: Symbols (recommended to not translate) */
+#define CTR_DICT_VAR                             ">>"                        // Word or symbol to declare a variable
+#define CTR_DICT_BULLET                          "~"                         // Adds a value to a list (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_SYMBOL_EQUALS                   "="                         // Compares the value of two objects (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_PLUS                            "+"                         // Symbol for addition (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_MINUS                           "-"                         // Symbol for subtraction (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_MULTIPLIER                      "×"                         // Non-ASCII symbol for multiplication (ASCII equivalent is provided by default) (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_DIVISION                        "÷"                         // Non-ASCII symbol for division (ASCII equivalent is provided by default) (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_GREATER                         ">"                         // Symbol for greater than comparisons
+#define CTR_DICT_LESS                            "<"                         // Symbol for less than comparisons
+#define CTR_DICT_AT_SYMBOL                       "?"                         // Symbol to get element from list or dict (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_NEW_ARRAY_AND_PUSH_SYMBOL       "←"                         // Non-ASCII symbol for return (ASCII equivalent is provided by default)
+#define CTR_DICT_GREATER_OR_EQUAL_SYMBOL         "≥"                         // Non-ASCII symbol for greater or equal comparisons (ASCII equivalent is provided by default) (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_LESS_OR_EQUAL_SYMBOL            "≤"                         // Non-ASCII symbol for less or equal comparisons (ASCII equivalent is provided by default) (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_UNEQUALS_SYMBOL                 "≠"                         // Non-ASCII symbol for unequality in comparisons (ASCII equivalent is provided by default) (binary message, must be exactly 1 UTF8-codepoint)
+#define CTR_DICT_END_OF_LINE                     "."                         // End of line marker
+#define CTR_DICT_NUM_DEC_SEP                     "."                         // Symbol to use as decimal separator
+#define CTR_DICT_NUM_THO_SEP                     ","                         // Symbol to use as thousands separator
+#define CTR_DICT_QUOT_OPEN                       "['"                        // Start of literal string
+#define CTR_DICT_QUOT_CLOSE                      "']"                        // End of a literal string
+#define CTR_DICT_MESSAGE_CHAIN                   ","                         // Symbol to use for chaining multiple keyword messages, must be exactly 1 byte
+#define CTR_DICT_ASSIGN                          ":="                        // Symbol to use for assignment
+#define CTR_DICT_RETURN                          "<-"                        // Symbol to use as return
+#define CTR_DICT_PUSH_SYMBOL                     ";"                         // Symbol to add an element to a list
+
+/* Section E: Restricted (do not translate unless you know impact) */
+#define CTR_DICT_BLOCK_START                     "{"                         // Symbol to use to start a group of instructions (not translatable)
+#define CTR_DICT_BLOCK_END                       "}"                         // Symbol to use to close a group of instructions (not translatable)
+#define CTR_DICT_PARAMETER_PREFIX                ":"                         // Symbol to use as parameter prefix (not translatable)
+#define CTR_DICT_PAREN_OPEN                      "("                         // Symbol to use for starting an experssion (not translatable)
+#define CTR_DICT_PAREN_CLOSE                     ")"                         // Symbol to use for ending an experssion (not translatable)
